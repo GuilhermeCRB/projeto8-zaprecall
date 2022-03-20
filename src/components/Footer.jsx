1c76b3be3746ef5103f3af9d385a2ed7982238c1
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-03-20 20:29:08 -0300

    feat: deck result display

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,7 @@ export default function Footer({numberOfQuestion, questionsAnswered, remembered}
 
     return(
         <footer className={footerClass}>
-            {/* {questionsAnswered === numberOfQuestion ? (
-                <img src="./imgs/party.png" alt="" />
-                <p></p>
-            ) : ("")} */}
+            {questionsAnswered === numberOfQuestion ? (deckResult()) : ("")}
             <p>{questionsAnswered}/{numberOfQuestion} CONCLUÍDOS</p>
             <div className="footer_status-questions">
                 {remembered.map((icon => {
@@ -17,4 +14,37 @@ export default function Footer({numberOfQuestion, questionsAnswered, remembered}
             </div>
         </footer>
     );
+
+    function deckResult(){
+        const result = remembered.filter((answer) => {
+            console.log("entrei")
+            if(answer === "close-circle"){
+                return true;
+            }else{
+                return false;
+            }
+        })
+
+        if(result.length === 0){
+            return (
+                <>
+                    <div className="footer_result">
+                        <img src="./imgs/party.png" alt="" />
+                        <p>Parabéns!</p>
+                    </div>
+                    <p className="footer_result_message">Você não esqueceu de nenhum flashcard!</p>
+                </>
+            )
+        }else{
+            return (
+                <>
+                    <div className="footer_result">
+                        <img src="./imgs/sad.png" alt="" />
+                        <p>Putz...</p>
+                    </div>
+                    <p className="footer_result_message">Ainda faltam alguns... <br /> Mas não desanime!</p>
+                </>
+            )
+        }
+    }
 }
